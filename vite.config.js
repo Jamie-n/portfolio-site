579c10537e3877971a807ec57f4bde603c76c083
Jamie <jamieneighbours@gmail.com>
2023-02-13 12:37:38 +0000

    Add basic tailwind css build code

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true
+        }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css'],
             refresh: true,
         }),
     ],
+
 });
