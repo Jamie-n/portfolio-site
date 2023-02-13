73bc74a954f3428bc044a812cd1c73cb4e9bab9d
Jamie Neighbours <56030151+Jamie-n@users.noreply.github.com>
2023-02-13 12:47:14 +0000

    Switch from tailwind play cdn to built resources (#1)
    * Add basic tailwind css build code
    
    * import tailwind styling in resources/css/app.css

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,8 +1,11 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script src="https://cdn.tailwindcss.com"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+@vite('resources/css/app.css')
+
 <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter|IBM+Plex+Mono">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
