828e71f710ccd0b80c92eccf9fbe266d2bdf9ee8
Jamie <jamieneighbours@gmail.com>
2023-02-13 12:51:41 +0000

    Improve mobile styling on projects.blade.php
    - Switch portfolio images to use grid-cols-1 on mobile

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -9,7 +9,7 @@
     <x-fake-code-block tag="h4" class="mb-3">
         <h4 class="font-bold text-xl">Compute</h4>
     </x-fake-code-block>
-    <div class="grid grid-cols-2 gap-4 mb-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10">
         <img src="{{asset('images/projects/compute/compute_home.png')}}" class="rounded-xl" alt="Compute homepage">
         <img src="{{asset('images/projects/compute/compute_listing.png')}}" class="rounded-xl" alt="Compute homepage">
         <img src="{{asset('images/projects/compute/create_product_with_image.gif')}}" class="rounded-xl" alt="Compute homepage">
@@ -20,7 +20,7 @@
         <h4 class="font-bold text-xl">CHFT Patient Leaflet Repository</h4>
     </x-fake-code-block>
 
-    <div class="grid grid-cols-3 gap-4 mb-10">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-10">
         <img src="{{asset('images/projects/PLR/plr_home.png')}}" class="rounded-xl" alt="Patient leaflet repository">
         <img src="{{asset('images/projects/PLR/patient_leaflet_repository.png')}}" class="rounded-xl" alt="Patient leaflet repository">
         <img src="{{asset('images/projects/PLR/plr_search.png')}}" class="rounded-xl" alt="Patient leaflet repository">
@@ -30,7 +30,7 @@
         <h4 class="font-bold text-xl">CHFT CHuFT</h4>
     </x-fake-code-block>
 
-    <div class="grid grid-cols-2 gap-4 mb-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10">
         <img src="{{asset('images/projects/chuft/chuft.png')}}" class="rounded-xl" alt="CHFT CHuFT Site">
         <img src="{{asset('images/projects/chuft/chuft_mid.png')}}" class="rounded-xl" alt="CHFT CHuFT Site">
     </div>
@@ -40,7 +40,7 @@
     </x-fake-code-block>
 
     <img src="{{asset('images/projects/finder/finder-main.png')}}" class="rounded-xl mb-4" alt="Compute homepage">
-    <div class="grid grid-cols-2 gap-4 mb-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10">
         <img src="{{asset('images/projects/finder/finder-mid.png')}}" class="rounded-xl" alt="Compute homepage">
         <img src="{{asset('images/projects/finder/finder-base.png')}}" class="rounded-xl" alt="Compute homepage">
     </div>
